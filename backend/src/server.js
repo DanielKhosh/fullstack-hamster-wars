bdd97a67964207100f825c74b2688f637455163f
Daniel Khoshnejad <Danielkhosh9@gmail.com>
2022-02-20 15:09:24 +0100

    changed sendFile

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const staticFolder = path.join(__dirname, 'public')
 
 // install middleware
 app.use( cors())
-app.use(express.static(__dirname + '/build'))  
+app.use(express.static(__dirname + 'build'))  
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next()
@@ -31,5 +31,5 @@ app.use( express.static(path.resolve('build/') ));
 
 app.get('*', (req, res) => {
     console.log('* is being called...')
-    res.send(__dirname + '/build/index.html');    
+    res.sendFile(__dirname + '/build/index.html');    
 });
